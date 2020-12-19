f0ac95e4f8d0283429d2b1a3df4bba483057f6a9
yuhsuanchuang <ychuang8@asu.edu>
2020-12-20 03:07:12 +0800

    remove token

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const githubApi = axios.create({
   baseURL: "https://api.github.com",
-  headers: { Authorization: "Token ce0e3c3c766be493512ce054ae7dad1c1c80d64e" },
 });
 
 export default githubApi;
