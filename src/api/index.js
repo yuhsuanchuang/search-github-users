841b9b965b4a5783ecd80c3ad06fae0f7262acad
yuhsuanchuang <ychuang8@asu.edu>
2020-12-20 16:22:09 +0800

    update api

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const githubApi = axios.create({
   baseURL: "https://api.github.com",
   headers: {
+    Accept: "application/vnd.github.v3+json",
     Authorization: `token ${process.env.GITHUB_API_TOKEN}`,
   },
 });
